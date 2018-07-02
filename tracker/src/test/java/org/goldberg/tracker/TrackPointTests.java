c664998072ec84d648e336dd677efe7655aadf19
Allen Goldberg <atg@agoldberg.org>
2018-07-02 15:46:37 -0700

    starting some tests

---
+++ tracker/src/test/java/org/goldberg/tracker/TrackPointTests.java
@@ -0,0 +1,19 @@
+package org.goldberg.tracker;
+
+import org.junit.Test;
+
+public class TrackPointTests {
+	
+	
+	
+	@Test
+	public void testSmoothElevation1(){
+		GPSTrack gpsTrack = new GPSTrack();
+
+	
+		
+		
+	}
+	
+
+}
